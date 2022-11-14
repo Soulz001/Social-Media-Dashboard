08274911425a16302f366228eff07f7ae9453f67
Israel Solomon <solomon.isy@gmail.com>
2022-11-14 21:34:19 +0100

    Added files

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-const SWITCH = document.querySelector(".switch");
+ const SWITCH = document.querySelector(".switch");
 SWITCH.addEventListener('click', () => {
         SWITCH.classList.toggle("dark")
         document.querySelector('body').classList.toggle('dark')
         let items = document.querySelectorAll('article')
         items.forEach(item => item.classList.toggle('dark'))
-})
+}) 
+
+const hexColor = () => `#${Math.floor(Math.random() * 0xffffff).toString(16).padStart(6, "0")}`
+console.log(hexColor())
